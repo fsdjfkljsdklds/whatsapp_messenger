2111d55b4d0470dc68f98597092ff6acba05f236
김지한 <xelnaga456@naver.com>
2023-03-10 18:04:47 +0900

    Initial commit

---
+++ lib/common/widgets/short_h_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ShortHBar extends StatelessWidget {
+  const ShortHBar({supe, this.height, this.width, this.colorr.key});
+
+  final double? height;
+  final double? width;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_messenger/common/routes/routes.dart';
+import 'package:whatsapp_messenger/common/theme/dark_theme.dart';
+import 'package:whatsapp_messenger/common/theme/light_theme.dart';
+import 'package:whatsapp_messenger/feature/auth/controller/auth_controller.dart';
+import 'package:whatsapp_messenger/feature/auth/pages/user_info_page.dart';
+import 'package:whatsapp_messenger/feature/welcome/pages/welcome_page.dart';
+import 'package:whatsapp_messenger/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp Me',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      themeMode: ThemeMode.system,
+      home: const UserInfoPage(),
+      onGenerateRoute: Routes.onGenerateRoute,
+    );
+  }
+}
